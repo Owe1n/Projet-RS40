abc0f205c0c42d0547d1daff7ab3e9c54f225438
Owein <owein.dourneau@gmail.com>
2021-05-21 15:21:53 +0200

    Print PEMS

--- src/print_pems.py
+++ src/print_pems.py
@@ -18,4 +18,6 @@ SERVER_PRIVATE_KEY_FILENAME = RESOURCES_DIR + "server-private-key.pem"
 SERVER_CSR_FILENAME = RESOURCES_DIR + "server-csr.pem"
 SERVER_PUBLIC_KEY_FILENAME = RESOURCES_DIR + "server-public-key.pem"
 
-# A compléter
+certs = pem.parse_file(CA_PUBLIC_KEY_FILENAME)
+# certs = pem.parse_file(CA_PRIVATE_KEY_FILENAME)
+print(certs)
